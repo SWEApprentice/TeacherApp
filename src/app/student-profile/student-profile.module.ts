16e644d3695ab76cddd9610d66be3e4a5dda584e
Supphanut Thongrak <jameszaro01@gmail.com>
2019-04-04 11:34:58 +0700

    path

--- src/app/student-profile/student-profile.module.ts
+++ src/app/student-profile/student-profile.module.ts
@@ -11,7 +11,52 @@ const routes: Routes = [
   {
     path: '',
     component: StudentProfilePage
-  }
+  },
+  {
+    path: 'student-save',
+    children: [
+      {
+        path: '',
+        loadChildren: '../student-save/student-save.module#StudentSavePageModule'
+      }
+    ]
+  },
+  {
+    path: 'student-score',
+    children: [
+      {
+        path: '',
+        loadChildren: '../student-score/student-score.module#StudentScorePageModule'
+      }
+    ]
+  },
+  {
+    path: 'student-evo',
+    children: [
+      {
+        path: '',
+        loadChildren: '../student-evo/student-evo.module#StudentEvoPageModule'
+      }
+    ]
+  },
+  {
+    path: 'student-portfolio',
+    children: [
+      {
+        path: '',
+        loadChildren: '../student-portfolio/student-portfolio.module#StudentProfolioPageModule'
+      }
+    ]
+  },
+  {
+    path: 'profile-edit',
+    children: [
+      {
+        path: '',
+        loadChildren: '../profile-edit/profile-edit.module#ProfileEditfolioPageModule'
+      }
+    ]
+  },
 ];
 
 @NgModule({
